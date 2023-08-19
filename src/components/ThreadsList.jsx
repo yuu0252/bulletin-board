ae56f62b3e7288463fa2271d301844cdc557d367
yuu <yuu0252u@gmail.com>
2023-08-19 14:39:57 +0900

    cssをスレッド一覧に追加

--- src/components/ThreadsList.jsx
+++ src/components/ThreadsList.jsx
@@ -1,5 +1,33 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const StyledThreadList = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 30px;
+
+    h2 {
+        margin: 30px;
+    }
+
+    ul {
+        max-width: 500px;
+        width: 100%;
+        list-style: none;
+    }
+
+    li {
+        padding: 15px;
+        border: 3px solid lightgreen;
+        border-radius: 10px
+    }
+
+    li:not(:last-child) {
+        margin-bottom: 15px;
+    }
+`;
 
 export const ThreadsList = () => {
     const [threadList, setThreadList] = useState([]);
@@ -12,15 +40,15 @@ export const ThreadsList = () => {
     }, [])
 
     return (
-        <>
+        <StyledThreadList>
             <h2>スレッド一覧</h2>
-            <ul id="threadList">
+            <ul>
                 {threadList.map((thread) => {
                     return (
                         <li key={thread.id}>{thread.title}</li>
                     )
                 })}
             </ul>
-        </>
+        </StyledThreadList>
     )
 }
